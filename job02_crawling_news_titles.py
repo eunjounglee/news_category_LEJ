e70384d95c8e5c9e87a9a40b5e55d817e6528573
eunjounglee <wjddl137@gmail.com>
2026-03-30 17:18:36 +0900

    job2 update

---
+++ job02_crawling_news_titles.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
+
+category = ['Politics', 'Economics', 'Social', 'Culture', 'World', 'IT']
+
+options = ChromeOptions()
+options.add_argument('lang=ko_KR')
+#options.add_argument('headless')
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+my_section = 3 # 0: politics  1: Economics, 2: Social, 3: Social, 4: culture, 5: IT
+
+url = 'https://news.naver.com/section/10{}'.format(my_section)
+driver.get(url)
+
+# button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]'
+# for i in range(10):
+#     time.sleep(0.5)
+#     driver.find_element(By.XPATH, button_xpath).click()
+
+while True:
+    try:
+        button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]'
+        driver.find_element(By.XPATH, button_xpath).click()
+    except:
+        break
+
+time.sleep(1)
+
+#class 로 찾기
+title_tags = driver.find_element(By.CLASS_NAME, 'sa_text_strong')
+
+titles = []
+for title in title_tags:
+    titles.append(title.text)
+df_titles = pd.DataFrame(titles, columns=['title'])
+df_titles['category'] = category[my_section]
+
+print(df_titles.head())
+df_titles.info()
+df_titles.to_csv('./crawling_data/news_titles_{}.csv'.format(category[my_section]))
+
+#Xpath로 찾기
+# for i in range(1, 70):
+#     for j in range(1, 7):
+#         try:
+#             title_xpath = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i, j)
+#             title = driver.find_element(By.XPATH, title_xpath).text
+#             print(title)
+#         except:
+#             print(i, j)
+#             continue
